8af675a6b4dd34d8818c3353af3afaa35b6fc31f
akash <akash.tout@gmail.com>
2022-11-09 15:56:59 +0530

    change in model librarian

--- app/models/librarian.rb
+++ app/models/librarian.rb
@@ -1,7 +1,12 @@
 class Librarian < ApplicationRecord
+  
   has_many :books, dependent: :destroy
   has_many :borrows, dependent: :destroy
   validates :name,:email,:usertype, presence: true
   validates :email, uniqueness: true
-  validates :password, :presence => true, :length => {:within => 6..10}, :on => :create
+
+
+    validates :password, :presence => true,
+                       :length => {:within => 6..10},
+                       :on => :create
 end
